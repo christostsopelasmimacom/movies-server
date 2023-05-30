9b4e71e74df48f66f7829af619953d80d827ea51
Christos Tsopelas <christos.tsopelas@mimacom.com>
2023-05-30 13:50:09 +0300

    first commit

---
+++ src/app/movies.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+
+@Controller()
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get('/movie')
+  getMovies() {
+    return this.moviesService.getMovies();
+  }
+
+  @Get('/search')
+  searchMovies(@Query('q') query: string) {
+    return this.moviesService.searchMovies(query);
+  }
+}
